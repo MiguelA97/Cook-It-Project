52e2213479e9c02b365f33c4a039df1380067575
Miguel Achega <miguel.achega@linkconsulting.com>
2021-04-17 21:33:40 +0100

    drop tables script

---
+++ server/database/drops.sql
@@ -0,0 +1,7 @@
+--DROP TABLES
+
+drop table if exists USERS_RECIPES;
+drop table if exists INGREDIENT_DETAILS;
+drop table if exists RECIPE;
+drop table if exists USER_RECIPE_LIST;
+drop table if exists USERS;
